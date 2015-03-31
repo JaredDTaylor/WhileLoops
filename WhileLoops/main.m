ae7f4dfb9031157ccba659cf77d0209b78a89376
Jared <jaredtaylor@cbrc.net>
2015-03-31 10:48:37 -0400

    Initial Commit

---
+++ WhileLoops/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WhileLoops
+//
+//  Created by Jared Taylor on 3/31/15.
+//  Copyright (c) 2015 Jared Taylor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
